c8e188d649f074df096e170fa321d2aaab3caf5f
Dylan-Fulop <79945573+Dylan-Fulop@users.noreply.github.com>
2021-04-16 14:50:52 -0700

    Vaccine Entry Updates
    Added equals and tostring method to vaccine entry to make it easier to use

--- VaccineEntry.java
+++ VaccineEntry.java
@@ -1,15 +1,14 @@
 
 public class VaccineEntry {
-	
+
 	private String id;
 	private String lastName;
 	private String firstName;
 	private String type;
 	private String date;
 	private String location;
-	
+
 	public VaccineEntry(String id, String lastName, String firstName, String type, String date, String location) {
-		super();
 		this.id = id;
 		this.lastName = lastName;
 		this.firstName = firstName;
@@ -17,52 +16,59 @@ public class VaccineEntry {
 		this.date = date;
 		this.location = location;
 	}
-	
+
 	public String getId() {
 		return id;
 	}
-	
+
 	public void setId(String id) {
 		this.id = id;
 	}
-	
+
 	public String getLastName() {
 		return lastName;
 	}
-	
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
-	
+
 	public String getFirstName() {
 		return firstName;
 	}
-	
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
-	
+
 	public String getType() {
 		return type;
 	}
-	
+
 	public void setType(String type) {
 		this.type = type;
 	}
-	
+
 	public String getDate() {
 		return date;
 	}
 	public void setDate(String date) {
 		this.date = date;
 	}
-	
+
 	public String getLocation() {
 		return location;
 	}
-	
+
 	public void setLocation(String location) {
 		this.location = location;
 	}
-	
+
+	public String toString() {
+		return id + "," + lastName + "," + firstName + "," + type + "," + date + "," + location + "\n";
+	}
+	public boolean equals(Object o) {
+		return o.toString().equals(this.toString());
+	}
+
 }
